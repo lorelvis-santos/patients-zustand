5124462f017e83b65aa5ac73662459b53471a5ea
Lorelvis Santos <santoslorelvis@gmail.com>
2026-03-01 17:22:13 -0400

    feat: setup project structure with main PatientForm and PatientsList components

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,21 @@
+import PatientForm from "./components/PatientForm";
+import PatientsList from "./components/PatientsList";
+
 export default function App() {
   
   return (
     <>
-      <h1>Patients - Zustand</h1>
+      <div className="container mx-auto mt-20">
+        <h1 className="text-4xl font-black text-center md:w-2/3 md:mx-auto">
+          Seguimiento de Pacientes {''}
+          <span className="text-indigo-700">Veterinaria</span>
+        </h1>
+
+        <div className="mt-12 md:flex">
+          <PatientForm />
+          <PatientsList />
+        </div>
+      </div>
     </>
   )
 }

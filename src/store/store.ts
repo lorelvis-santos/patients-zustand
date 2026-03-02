4884ec32ccf22be5dd04f6a1c00637abdd550f26
Lorelvis Santos <santoslorelvis@gmail.com>
2026-03-02 06:53:22 -0400

    feat(store): add UUID generation for new patients and update patient store logic

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,12 +1,21 @@
 import { create } from "zustand";
 import type { DraftPatient, Patient } from "../types";
+import { v4 as uuidv4 } from "uuid";
 
 type PatientState = {
   patients: Patient[];
   addPatient: (data: DraftPatient) => void;
 };
 
-export const usePatientStore = create<PatientState>(() => ({
+const createPatient = (patient: DraftPatient): Patient => {
+  return { id: uuidv4(), ...patient };
+};
+
+export const usePatientStore = create<PatientState>((set) => ({
   patients: [],
-  addPatient: (data: DraftPatient) => console.log(data),
+  addPatient: (data: DraftPatient) => {
+    set((state) => ({
+      patients: [...state.patients, createPatient(data)],
+    }));
+  },
 }));
